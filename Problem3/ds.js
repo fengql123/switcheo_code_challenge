b0871f032142be21765b3eba106e3555a6dc6d12
fql <77008470+fengql123@users.noreply.github.com>
2022-04-20 12:00:23 +0800

    done question 3

--- Problem3/ds.js
+++ Problem3/ds.js
@@ -1,7 +1,26 @@
+import axios from "axios";
+class price {
+  constructor(price) {
+    this.price = price;
+  }
+
+  mid() {
+    return (this.price.buy + this.price.sell) / 2 / 100;
+  }
+
+  quote() {
+    return this.price.pair.slice(3, 6);
+  }
+}
+class prices {
+  constructor(prices) {
+    this.prices = prices.map((p) => new price(p));
+  }
+}
 class dataSource {
   async getPrices() {
-    const response = await fetch("https://static.ngnrs.io/test/prices");
-    return response.data.prices;
+    const response = await axios.get("https://static.ngnrs.io/test/prices");
+    return new prices(response.data.data.prices).prices;
   }
 }
 
@@ -11,9 +30,7 @@ ds.getPrices()
   .then((prices) => {
     prices.forEach((price) => {
       console.log(
-        `Mid price for ${price.pair} is ${
-          (price.buy + price.sell) / 2 / 100
-        } ${price.pair.slice(3, 6)}.`
+        `Mid price for ${price.pair} is ${price.mid()} ${price.quote()}.`
       );
     });
   })
